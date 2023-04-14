e51827a3d8fed671c6499827fcfd31c12d9081f7
Eli de Smet <e.desmet@exner.nl>
2023-04-14 13:36:48 +0200

    feat: add nvim installer

---
+++ install_nvim.sh
@@ -0,0 +1,3 @@
+wget -q --show-progress -O nvim.appimage "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
